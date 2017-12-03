927a28b459d06e5539e7ddd952c92a887b8002ea
Nicholas Echevarria <nick.echev@gmail.com>
2017-12-03 00:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function deleteFromObjectByKey(object, key) {
   delete newObject[key]
   return newObject
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
